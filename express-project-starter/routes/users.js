e14c9c184d986b9796b88c257f75822446c723f7
Megan McKenna <meganemckenna1@gmail.com>
2021-12-13 18:16:17 -0600

    completed user signup logic

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,113 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('../utils');
+
+const router = express.Router();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', csrfProtection, function(req, res, next) {
+  const user = db.User.build();
+  res.render('user-signup', {
+    title: 'Sign Up',
+    user,
+    csrfToken: req.csrfToken()
+  });
 });
+
+const userValidators = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 50 })
+    .withMessage('First Name must not be more than 50 characters long'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username')
+    .isLength({ max: 50 })
+    .withMessage('Username must not be more than 50 characters long')
+    .custom((value) => {
+      return db.User.findOne({ where: { username: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Username is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ min: 8 })
+    .withMessage('Confirm Password must be at least 8 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
+];
+
+
+router.post('/signup', csrfProtection, userValidators, asyncHandler(async (req, res, next) => {
+  const { firstName, lastName, email, username, password } = req.body;
+
+  const user = db.User.build({
+    fullName: `${firstName.charAt(0).toUpperCase() + firstName.slice(1)} ${lastName.charAt(0).toUpperCase() + lastName.slice(1)}`,
+    email,
+    username
+  });
+
+  const validatorErrors = validationResult(req);
+
+  if(validatorErrors.isEmpty()) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashedPassword;
+    console.log("HELLOO")
+    await user.save();
+    //TODO add loginUser
+    res.redirect('/');
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    console.log("GOODBYE")
+    res.render('user-signup', {
+      title: 'Sign Up',
+      user,
+      errors,
+      firstName,
+      lastName,
+      email,
+      username,
+      csrfToken: req.csrfToken(),
+    });
+  }
+}));
 
 module.exports = router;
