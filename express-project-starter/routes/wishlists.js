e940ea8d91821c8785b9a083ed987f769a2cc949
sornamv <sornamvairavan@outlook.com>
2021-12-15 11:49:00 -0800

    updated wishlists/id route

--- express-project-starter/routes/wishlists.js
+++ express-project-starter/routes/wishlists.js
@@ -3,6 +3,7 @@ const express = require('express');
 const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('../utils');
 const { requireAuth } = require('../auth');
+const { Op } = require("sequelize")
 
 const router = express.Router();
 
@@ -10,10 +11,17 @@ router.use(requireAuth);
 
 router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     const wishlist = await db.Wishlist.findByPk(req.params.id);
-
+    console.log(req.session.auth.userId)
+    const wishlistsByUser = await db.Wishlist.findAll({
+        where: {
+            userId: req.session.auth.userId
+        }
+    })
+    console.log("WISHLISTTTT", wishlistsByUser)
     res.render('wishlist', {
         title: wishlist.name,
-        
+        wishlistsByUser
+
     })
 }));
 
