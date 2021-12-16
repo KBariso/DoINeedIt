aa4ecacc7ef7141d6dad1d6edcd57e09cf7603c6
kielbariso@gmail.com <kielbariso@gmail.com>
2021-12-16 13:19:13 -0600

    editing the wishlists routes, bug in the post edit

--- routes/wishlists.js
+++ routes/wishlists.js
@@ -1,101 +1,145 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../db/models');
-const { csrfProtection, asyncHandler } = require('../utils');
-const { requireAuth, isAuthorized } = require('../auth');
-const { check, validationResult } = require('express-validator');
+const db = require("../db/models");
+const { csrfProtection, asyncHandler } = require("../utils");
+const { requireAuth, isAuthorized } = require("../auth");
+const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
 router.use(requireAuth);
 
-
 /* GET Wishlists page by Id. */
 
-router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
-
+router.get(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res) => {
     const wishlist = await db.Wishlist.findByPk(req.params.id, {
-        include: {
-            all: true
-        }
+      include: {
+        all: true,
+      },
     });
 
     const wishlistsByUser = await db.Wishlist.findAll({
-        where: {
-            userId: req.session.auth.userId
-        },
+      where: {
+        userId: req.session.auth.userId,
+      },
+    });
 
-    })
+    const authorized = isAuthorized(req.session.auth.userId, wishlist.userId);
 
-    const authorized = isAuthorized(req.session.auth.userId, wishlist.userId)
-
-    res.render('wishlist', {
-        title: wishlist.name,
-        wishlistsByUser,
-        wishlist,
-        items: wishlist.Items,
-        authorized
-    })
-
-}));
+    res.render("wishlist", {
+      title: wishlist.name,
+      wishlistsByUser,
+      wishlist,
+      items: wishlist.Items,
+      authorized,
+    });
+  })
+);
 
 /* GET page to create a new Wishlist. */
 
-router.get('/new', csrfProtection, (req, res) => {
-    const wishlist = db.Wishlist.build()
-    res.render('new-wishlist', {
-        title: "Create Wishlist",
-        wishlist,
-        csrfToken: req.csrfToken()
-    })
-})
-
+router.get("/new", csrfProtection, (req, res) => {
+  const wishlist = db.Wishlist.build();
+  res.render("new-wishlist", {
+    title: "Create Wishlist",
+    wishlist,
+    csrfToken: req.csrfToken(),
+  });
+});
 
 const wishlistValidators = [
-    check('name')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a name for the Wishlist')
-        .isLength({ max: 100 })
-        .withMessage('Name must not be more than 100 characters long'),
-    check('isPublic')
-        .exists({ checkFalsy: true })
-        .withMessage('Please verify if you want to make your Wishlist public')
-        .isBoolean()
-        .withMessage('Privacy setting must be either Public or Private')
-]
-
+  check("name")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a name for the Wishlist")
+    .isLength({ max: 100 })
+    .withMessage("Name must not be more than 100 characters long"),
+  check("isPublic")
+    .exists({ checkFalsy: true })
+    .withMessage("Please verify if you want to make your Wishlist public")
+    .isBoolean()
+    .withMessage("Privacy setting must be either Public or Private"),
+];
 
 /* POST a new Wishlist. */
-router.post('/new', csrfProtection, wishlistValidators, asyncHandler(async (req, res) => {
-    let {name, isPublic, description} = req.body;
-    const validatorErrors = validationResult(req)
+router.post(
+  "/new",
+  csrfProtection,
+  wishlistValidators,
+  asyncHandler(async (req, res) => {
+    let { name, isPublic, description } = req.body;
+    const validatorErrors = validationResult(req);
 
     if (validatorErrors.isEmpty()) {
-        let newWishlist = await db.Wishlist.create({
-            name,
-            isPublic,
-            userId: req.session.auth.userId,
-            description
-        })
-        res.redirect(`/wishlists/${newWishlist.id}`)
+      let newWishlist = await db.Wishlist.create({
+        name,
+        isPublic,
+        userId: req.session.auth.userId,
+        description,
+      });
+      res.redirect(`/wishlists/${newWishlist.id}`);
     } else {
-        const errors = validatorErrors.array().map(error => error.msg)
-        res.render('new-wishlist', {
-            title: "Create Wishlist",
-            name,
-            isPublic,
-            description,
-            errors,
-            csrfToken: req.csrfToken()
-        })
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render("new-wishlist", {
+        title: "Create Wishlist",
+        name,
+        isPublic,
+        description,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
     }
-}))
+  })
+);
 
 /* PUT Wishlists page by Id */
+router.get(
+  "/:id/edit",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const wishlist = await db.Wishlist.findByPk(req.params.id, {
+      include: {
+        all: true,
+      },
+    });
 
+    const wishlistsByUser = await db.Wishlist.findAll({
+      where: {
+        userId: req.session.auth.userId,
+      },
+    });
+
+    const authorized = isAuthorized(req.session.auth.userId, wishlist.userId);
+
+    res.render("edit-wishlist", {
+      title: "Edit Wishlist",
+      wishlistsByUser,
+      wishlist,
+      items: wishlist.Items,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
+
+router.post("/:id", csrfProtection, (req, res) => {
+  console.log("hello");
+  res.send("hello");
+});
+
+// router.post(
+//   "/:id/edit",
+//   csrfProtection,
+//   asyncHandler(async (req, res) => {
+//     console.log(req.params.id)
+//     const wishlist = await db.Wishlist.findByPk(req.params.id);
+//     const { name, description, isPublic } = req.body;
+//     await wishlist.update( {name, description, isPublic} )
+//     await wishlist.save()
+//     res.redirect(`/`)
+//   })
+// );
 
 /* POST Comments on Wishlists by Id. */
-
-
 
 module.exports = router;
